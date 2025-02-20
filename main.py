4f2c40547614ca131b2fbff1fa3373c8f05eb7a3
Shutt3rr <bryanmora8114@gmail.com>
2025-02-20 08:28:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import requests 
+from PIL import Image
 
+def get_dog():
+  url = "https://api.thedogapi.com/v1/images/search"
+  response = requests.get(url)
+  print(response)
+  data = response.json()
+  print(data)
+  image = requests.get(data[0]["url"])
+  img = Image.open(BytesIO(image.content))
+  title = "Random Dog"
+  img.save(f"./doggy/{title}.jpg")
+
+get_dog()
 
 
